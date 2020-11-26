bd5285c2f5d7e6c704685cf101d6e8ead5bc74ad
JaimeLeyva <45579126+JaimeLeyva@users.noreply.github.com>
2020-11-25 17:14:50 -0700

    Descompresion Archivos

---
+++ Punto de venta/src/punto/de/venta/DBConexion.java
@@ -0,0 +1,44 @@
+package punto.de.venta;
+
+import static java.lang.Class.forName;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ * @author Rey Figuritas
+ */
+public class DBConexion {
+
+    private static String DRIVER = "com.mysql.jdbc.Driver";
+    private static String USUARIO = "root";
+    private static String PASSWORD = "Tuerto.21";
+    private static String URL = "jdbc:mysql://localhost:3306/testpuntoventa";
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException E) {
+            JOptionPane.showMessageDialog(null, "Error en el Driver" + E);
+        }
+
+    }
+
+    public Connection getConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+            //JOptionPane.showMessageDialog(null, "Conexion Exitosa ");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Conexion Denegada " + e);
+        }
+        return con;
+    }
+
+    Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
